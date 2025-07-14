8068667dfe51fdf463975fcb6d4adb26a2818751
jeoniltae <foodchain@naver.com>
2025-07-14 23:09:13 +0900

    feat: style 공통 요소 정의 및 로그인 화면 작업

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,12 @@
 /**
  * Javascript Document UI
  */
+const commonUi = {
+  init: () => {
+    // User-Agent 확인
+    const ua = navigator.userAgent;
+    ua.indexOf('Edg') > -1 && document.documentElement.classList.add('is-edge-browser');
+  },
+};
+
+document.addEventListener('DOMContentLoaded', commonUi.init);
