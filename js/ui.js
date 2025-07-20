5a69ba7d4f35d7225fd067cd70742081b8b6b0d5
jeoniltae <foodchain@naver.com>
2025-07-20 23:26:21 +0900

    feat: 카메라 관리 화면 작업

--- js/ui.js
+++ js/ui.js
@@ -3,6 +3,11 @@
  */
 const commonUi = {
   init: () => {
+    // HTML include (퍼블리싱 확인용)
+    if (document.querySelectorAll('[include-html]').length !== 0) {
+      includeHTML();
+    }
+
     // User-Agent 확인
     const ua = navigator.userAgent;
     ua.indexOf('Edg') > -1 && document.documentElement.classList.add('is-edge-browser');
